d281e221349169dab420f6e9c8c6e5043ccd8d0f
bs <26665808+bs-public@users.noreply.github.com>
2023-11-13 14:24:38 +0530

    Logging with SLF4j

---
+++ README.md
@@ -0,0 +1,4 @@
+### Workspace for Java
+- [design-patterns](https://github.com/bs-public/ws-java/tree/main/design-patterns)
+- [logging-slf4j](https://github.com/bs-public/ws-java/tree/main/logging-slf4j)
+
---
+++ logging-slf4j/src/main/java/com/App.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+  private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+  public static void main(String[] args) {
+    logger.info(" SLF4J: Simple Logging Facade for Java");
+  }
+
+}
